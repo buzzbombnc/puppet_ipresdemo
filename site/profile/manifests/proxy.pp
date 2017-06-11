9fa6ab7c5bdef7d8ab8389dfeadbb6461423c9ce
buzzbombnc <buzzbomb-github@buzzbomb.net>
2017-06-10 20:11:09 -0400

    Remove default vhost and create a custom proxy one.

--- site/profile/manifests/proxy.pp
+++ site/profile/manifests/proxy.pp
@@ -2,8 +2,17 @@ class profile::proxy (
     String $cluster_name
 )
 {
-    class { "apache" : }
+    class { "apache" :
+        default_vhost => false,
+    }
     class { "apache::mod::proxy" : }
+    apache::vhost { "default_proxy":
+        port => 80,
+        docroot => '/var/www/html',
+        proxy_pass => [
+            { 'path' => '/app', 'url' => "balancer://$cluster_name" }
+        ],
+    }
 
     apache::balancer { "$cluster_name":
         require => Class['apache::mod::proxy'],
